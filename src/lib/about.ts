fd49669ba9aa0fb7458acd0b2a70c779024bd080
Jonathan Gamble <jdgamble555@gmail.com>
2024-05-31 22:09:07 -0500

    test

--- src/lib/about.ts
+++ src/lib/about.ts
@@ -1,5 +1,6 @@
 import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
 import { app } from "./firebase";
+import { getAuth } from "firebase/auth";
 
 type AboutDoc = {
     name: string;
@@ -9,6 +10,8 @@ type AboutDoc = {
 export const getAbout = async () =>{
 
     const db = getFirestore(app);
+
+    const auth = getAuth(app);
 
     const aboutSnap = await getDoc(
         doc(db, '/about/ZlNJrKd6LcATycPRmBPA')
