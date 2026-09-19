c58a6334ee98cf15ce40e825b784222c9434ded3
Jonathan Gamble <jdgamble555@gmail.com>
2026-09-19 15:47:29 -0500

    qwik update

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -10,10 +10,17 @@ export default component$(() => {
     return (
         <div>
             <div class="flex flex-col gap-3">
-                {todos.value.loading ? <p>Loading todos...</p> : todos.value.error ? <p role="alert" class="text-red-600">{todos.value.error}</p> : todos.value.data.length
-                    ? todos.value.data.map((todo) => <Todo key={todo.id} todo={todo} />)
-                    : <p><b>Add your first todo item!</b></p>
-                }
+                {todos.value.loading ? (
+                    <p>Loading todos...</p>
+                ) : todos.value.error ? (
+                    <p role="alert" class="text-red-600">
+                        {todos.value.error}
+                    </p>
+                ) : todos.value.data.length ? (
+                    todos.value.data.map((todo) => <Todo key={todo.id} todo={todo} />)
+                ) : (
+                    <p><b>Add your first todo item!</b></p>
+                )}
             </div>
             <TodoForm />
         </div>
@@ -40,16 +47,27 @@ export const TodoForm = component$(() => {
 
     return (
         <div class="mt-5">
-            <form class="flex gap-3 items-center justify-center" preventdefault:submit onSubmit$={onSubmit}>
+            <form
+                class="flex gap-3 items-center justify-center"
+                preventdefault:submit
+                onSubmit$={onSubmit}
+            >
                 <input
                     class="border p-2 rounded-lg"
                     bind:value={text}
                 />
-                <button class="border p-2 rounded-lg bg-purple-600 text-white font-semibold" type="submit">
+                <button
+                    class="border p-2 rounded-lg bg-purple-600 text-white font-semibold"
+                    type="submit"
+                >
                     Add Task
                 </button>
             </form>
-            {error.value && <p role="alert" class="mt-2 text-center text-red-600">{error.value}</p>}
+            {error.value && (
+                <p role="alert" class="mt-2 text-center text-red-600">
+                    {error.value}
+                </p>
+            )}
         </div>
     );
 });
